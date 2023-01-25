bb8e89bebb56fbcd30491908acca2e8efc2ba278
tomoki maeda <maetomoda@gmail.com>
2023-01-25 17:03:52 +0900

    test

--- 第1回/index.js
+++ 第1回/index.js
@@ -1,9 +1,27 @@
 const btn = document.querySelector("button")
 
+// 様々な↓　DOM　のaddeventlistener の使いかた
+/* document.addEventListener("on-click", test()) */
+/* document.addEventListener("mousedown", test2()) */
+document.addEventListener("DOMContentLoaded", test1())
+/* document.querySelector("button").addEventListener(focus, alert("クリックされてます")) */
 
-document.addEventListener("on-click", test())
+let button = document.getElementById("click1");
+button.addEventListener("click", hello());
+
+
 
 
 function test() {
   alert("lsah;lhlaflgias;oi")
 }
+function test1() {
+  alert("ちゃんと読み込まれましたよ")
+}
+function test2() {
+  alert("マウスボタンを押している時")
+}
+function hello() {
+  alert("こんにちは")
+}
+
