9b618f1e36c5a2efd6d837e4dab6659e7800c17b
tomoki maeda <maetomoda@gmail.com>
2023-01-27 19:31:55 +0900

    innerTHMLの使い方

---
+++ 第2回/index.js
@@ -0,0 +1,23 @@
+
+
+let test = document.querySelector("h1")
+console.log(test)
+
+
+
+
+
+// // innerHTMLの使いかた
+// element.innerHTML = 'value';
+
+
+// let element = document.querySelector("p");
+// ↑　ここで使う要素の指定（書き方は覚える）
+
+// element.innerHTML = "my name is tanaka";
+// element.innerHTML = "value";
+// ↑
+
+
+
+
