4c43fb13197f99aa00009fb9435efee18bce07fa
tomoki maeda <maetomoda@gmail.com>
2023-01-27 15:50:42 +0900

    tedt

--- 第1回/index.js
+++ 第1回/index.js
@@ -1,16 +1,34 @@
-const btn = document.querySelector("button")
+const btn = document.querySelectorAll("button");
 
 // 様々な↓　DOM　のaddeventlistener の使いかた
 /* document.addEventListener("on-click", test()) */
 /* document.addEventListener("mousedown", test2()) */
-document.addEventListener("DOMContentLoaded", test1())
+// document.addEventListener("DOMContentLoaded", test1())
 /* document.querySelector("button").addEventListener(focus, alert("クリックされてます")) */
 
-let button = document.getElementById("click1");
-button.addEventListener("click", hello());
+btn.forEach((element) => {
+  element.addEventListener('click', hello);
+});
+
+document.querySelector('#atagu').addEventListener('click', function(e) {
+  console.log(e.target.innerHTML);
+  window.alert()
+  e.preventDefault();
+  location.href = 'https://google.com';
+})
+
+// (function() {
+//   // window.val = 9999;
+//   val = 9999;
+
+// })()
+
+// console.log(val);
 
 
-
+// let button = document.getElementById('click1');
+// // console.log(button, 7777)
+// button.addEventListener("click",hello);
 
 function test() {
   alert("lsah;lhlaflgias;oi")
@@ -21,7 +39,9 @@ function test1() {
 function test2() {
   alert("マウスボタンを押している時")
 }
-function hello() {
-  alert("こんにちは")
+function hello(event) {
+  console.log(event.target.innerHTML)
+  alert()
+
 }
 
